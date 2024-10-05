df03761ce82300dc2298b32efe462f9dfc865257
EsrakFahim <efahim0656@gmail.com>
2024-10-05 16:13:28 +0600

    Refactor CSS styles and remove unused code

--- src/app/write/page.js
+++ src/app/write/page.js
@@ -4,10 +4,16 @@ import React, { useEffect, useState } from "react";
 import TextEditor from "@/Components/TextEditor/TextEditor";
 import { useForm } from "react-hook-form";
 import TagsEditor from "@/Components/TagsEditor/TagsEditor";
+import Image from "next/image";
+import axios from "axios";
+import { MdOutlineContentCopy } from "react-icons/md";
+import toast from "react-hot-toast";
 
 const Page = ({ placeholder }) => {
       const [content, setContent] = useState("");
       const [productTags, setProductTags] = useState([]);
+      const [imageSrc, setImageSrc] = useState("");
+      const [imgLiveUrl, setImgLiveUrl] = useState("");
       const {
             register,
             handleSubmit,
@@ -69,9 +75,53 @@ const Page = ({ placeholder }) => {
             reset();
       };
 
+      const handleUploadClick = () => {
+            document.getElementById("file-upload").click();
+      };
+
+      const handleImageUrl = async (url) => {
+            // const ref = await axios.post(
+            //       "http://localhost:5000/api/v1/image/upload"
+            // );
+            setImgLiveUrl(url.slice(0, 5));
+            // console.log(ref);
+      };
+
+      // img render
+      const handleFileChange = (event) => {
+            if (event.target.files && event.target.files[0]) {
+                  const reader = new FileReader();
+
+                  reader.onload = (e) => {
+                        setImageSrc(e.target.result);
+                        if (e.target.result) {
+                              handleImageUrl(e.target.result);
+                        } else {
+                              console.log("image is not available");
+                        }
+                  };
+
+                  reader.readAsDataURL(event.target.files[0]);
+            }
+      };
+
+      const handleCopy = () => {
+            navigator.clipboard
+                  .writeText(imgLiveUrl)
+                  .then(() => {
+                        // alert("Text copied to clipboard!");
+                        toast("Text copied to clipboard!");
+                  })
+                  .catch((err) => {
+                        console.error("Failed to copy text: ", err);
+                  });
+      };
       return (
-            <div className="max-w-[70%] mx-auto py-10">
-                  <form onSubmit={handleSubmit(handleData)}>
+            <div className="w-full lg:w-[75%] mx-auto py-10 px-10 flex flex-col lg:flex-row items-start gap-5">
+                  <form
+                        className="w-full lg:w-[70%]"
+                        onSubmit={handleSubmit(handleData)}
+                  >
                         {/* Title field */}
                         <div className="mb-5 py-5 flex flex-col items-start justify-start gap-6">
                               <label
@@ -134,6 +184,70 @@ const Page = ({ placeholder }) => {
                               )}
                         </div>
 
+                        {/* Image input field */}
+                        <div className="mb-5 py-5 flex flex-col items-start justify-start gap-6">
+                              {/* remember this field take an image and return an image url  */}
+                              <label
+                                    htmlFor="blog_Subtitle"
+                                    className="text-black ml-1 text-[1.3em] leading-8"
+                              >
+                                    Blog Image{" "}
+                                    <span className="text-sm text-gray-400">
+                                          (It gives a url and use that url into
+                                          your blog)
+                                    </span>
+                              </label>
+                              <div
+                                    className="rounded-md py-2 border-dashed border-2 border-gray-300 flex items-center justify-center cursor-pointer"
+                                    onChange={handleFileChange}
+                              >
+                                    <div className="avatar-wrapper flex items-center justify-center">
+                                          {imageSrc ? (
+                                                <div className="avatar">
+                                                      <div className="w-24 rounded-full">
+                                                            <Image
+                                                                  className="profile-pic object-cover"
+                                                                  src={imageSrc}
+                                                                  alt="Profile Pic"
+                                                                  width={100}
+                                                                  height={100}
+                                                            />
+                                                      </div>
+                                                </div>
+                                          ) : (
+                                                <div
+                                                      className="upload-button flex flex-col items-center gap-2 text-gray-400"
+                                                      onClick={
+                                                            handleUploadClick
+                                                      }
+                                                >
+                                                      <h5>upload your photo</h5>
+                                                </div>
+                                          )}
+                                          <input
+                                                type="file"
+                                                id="file-upload"
+                                                accept="image/*"
+                                                className="file-upload"
+                                                style={{
+                                                      display: "none",
+                                                }}
+                                          />
+                                    </div>
+                              </div>
+                              <div>
+                                    {imgLiveUrl && (
+                                          <p
+                                                onClick={handleCopy}
+                                                className="text-gray-500 flex items-center gap-3 cursor-pointer border border-gray-300 rounded-md p-2"
+                                          >
+                                                {imgLiveUrl}
+                                                <MdOutlineContentCopy />
+                                          </p>
+                                    )}
+                              </div>
+                        </div>
+
                         {/* Content field */}
                         <TextEditor
                               placeholder={placeholder}
@@ -141,8 +255,26 @@ const Page = ({ placeholder }) => {
                               setContent={setContent}
                         />
 
+                        {/* Submit button */}
+                        <button
+                              type="submit"
+                              className="bg-blue-500 text-white px-4 py-2 rounded-md mt-5"
+                        >
+                              Submit
+                        </button>
+                  </form>
+                  <div className="w-full lg:w-[30%]">
                         {/* Tags field */}
                         <div>
+                              <div>
+                                    <label className="text-black ml-1 text-[1.3em] leading-8">
+                                          Tags
+                                          <span className="text-sm text-gray-400">
+                                                {" "}
+                                                (Press enter to add tags)
+                                          </span>
+                                    </label>
+                              </div>
                               <TagsEditor
                                     selectedTags={() => {}}
                                     tags={productTags}
@@ -162,15 +294,7 @@ const Page = ({ placeholder }) => {
                                     }
                               />
                         </div>
-
-                        {/* Submit button */}
-                        <button
-                              type="submit"
-                              className="bg-blue-500 text-white px-4 py-2 rounded-md mt-5"
-                        >
-                              Submit
-                        </button>
-                  </form>
+                  </div>
             </div>
       );
 };
