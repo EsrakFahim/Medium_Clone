3e70cfccd8eadb1db78d2d8afbebf6da4177177f
EsrakFahim <efahim0656@gmail.com>
2024-10-12 11:58:49 +0600

    Refactor next.config.mjs to add eslint configuration for ignoring builds

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
       images: {
             domains: ["res.cloudinary.com"], // Add 'res.cloudinary.com' to the allowed domains
       },
+      eslint: {
+            ignoreDuringBuilds: true,
+      },
 };
 
 export default nextConfig;
